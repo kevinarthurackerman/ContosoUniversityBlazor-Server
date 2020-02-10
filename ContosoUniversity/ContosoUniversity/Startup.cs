f3aa7bad31557cb37631b717a6705d6da026fc73
kackerman <kevin.arthur.ackerman@gmail.com>
2020-02-10 00:40:57 -0600

    Copies over pages from ContosoUniversityDotNetCore-Pages example
    Many things are still broken - this is just where I was first able to get it to run okay
    Specifically, I know for sure there is an issue with the form group and display bindings, validation, and links could be generated with a helper rather than text string manipulation

--- ContosoUniversity/ContosoUniversity/Startup.cs
+++ ContosoUniversity/ContosoUniversity/Startup.cs
@@ -1,15 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+using MediatR;
+using ContosoUniversity.Infrastructure;
+using FluentValidation.AspNetCore;
+using AutoMapper;
 
 namespace ContosoUniversity
 {
@@ -22,16 +21,31 @@ namespace ContosoUniversity
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddMediatR(typeof(Startup));
+            services.AddScoped(
+                typeof(IPipelineBehavior<,>),
+                typeof(TransactionBehavior<,>));
+            services.AddScoped(
+                typeof(IPipelineBehavior<,>),
+                typeof(LoggingBehavior<,>));
+
+            services.AddRazorPages(opt =>
+                {
+                    opt.Conventions.ConfigureFilter(new DbContextTransactionPageFilter());
+                    opt.Conventions.ConfigureFilter(new ValidatorPageFilter());
+                })
+                .AddFluentValidation(cfg => { cfg.RegisterValidatorsFromAssemblyContaining<Startup>(); });
+
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,7 +55,6 @@ namespace ContosoUniversity
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
